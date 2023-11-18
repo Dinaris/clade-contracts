11f65a3606fa8cf8c086e28173683bfed746a661
Olçar Çınar <cinax@usa.com>
2023-11-18 10:38:07 +0300

    idk what im doing now

--- contracts/core/Dao.sol
+++ contracts/core/Dao.sol
@@ -3,12 +3,63 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/utils/Context.sol";
 
+contract SessionBlock is Context {
+    uint256 public proposalId;
+    uint256 public createTime;
+    uint256 public startTime;
+    uint256 public endTime;
+    uint256 public status;
+
+    address[] public members;
+    address[] public voices;
+    address[] public moderators;
+}
+
+contract ProposalBlock is Context {
+
+    uint256 public id;
+    string public name;
+    string public description;
+    string public document; //ipfs
+    uint256 public createTime;
+    uint256 public startTime;
+    uint256 public endTime;
+    uint256 public status;
+    address public requester;
+    address public dao;
+
+    //Votes
+    address[] public votes;
+    mapping(address => bool) public sessionVotes;
+
+    //Speakers
+    address[] public speakers;
+    mapping(address => bool) public sessionSpeakers;
+
+    //Moderators
+    address[] public moderators;
+    mapping(address => bool) public sessionModerators;
+}
+
 contract Dao is Context {
 
     string public name;
     string public symbol;
     string public logo;
     string public description;
+
+    //Members
+    struct MemberInfo {
+        address user;
+        uint256 userType;
+        uint256 level;
+        uint256 status;
+        uint256 createTime;
+        uint256 updateTime;
+    }
+
+    mapping(uint256 => SessionBlock) public sessions;
+    mapping(uint256 => ProposalBlock) public proposals;
 
     constructor(
         string memory _name,
@@ -20,15 +71,6 @@ contract Dao is Context {
         symbol = _symbol;
         logo = _logo;
         description = _description;
-    }
-
-    struct MemberInfo {
-        address user;
-        uint256 userType;
-        uint256 level;
-        uint256 status;
-        uint256 createTime;
-        uint256 updateTime;
     }
 
     mapping(address => MemberInfo) public members;
--- contracts/core/SessionManager.sol
+++ contracts/core/SessionManager.sol
@@ -12,25 +12,4 @@ contract SessionManager is Context {
     constructor(Dao manager) {
         manager = manager;
     }
-
-    struct SessionBlock {
-        uint256 id;
-        SessionInfo info;
-        SessionMembers members;
-    }
-
-    struct SessionInfo {
-        uint256 createTime;
-        uint256 startTime;
-        uint256 endTime;
-        uint256 status;
-    }
-
-    struct SessionMembers {
-        address[] members;
-        address[] voices;
-        address[] moderators;
-    }
-
-    mapping(address => mapping(uint256 => SessionBlock)) public sessions;
 }
